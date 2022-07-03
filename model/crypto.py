4f218a5d66b87273f2c2b55f40ec055860e4a4bb
nakagawa <nakagawa.atsushi.1998@gmail.com>
2022-07-03 23:42:22 +0900

    データへの行ごとの追加&パラボリックSAR&DMI

--- model/crypto.py
+++ model/crypto.py
@@ -1,72 +1,242 @@
+from xml.sax.handler import property_dom_node
+from xmlrpc.client import _datetime_type
 import pandas as pd
 
 class Crypto:
     class Column:
-        candle = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-        bb = ['Date', 'SMA', 'Std', '+2σ', '-2σ']
-        rsi = ['Date', 'Diff', '+Ave', '-Ave', 'RSI']
-        macd = ['Date', 'ShortMA', 'LongMA', 'MACD', 'Signal', 'Hist', 'HistRSI']
+        ohlcv = [
+            'Datetime', #日時
+            'Open', #始値
+            'High', #高音
+            'Low',  #安値
+            'Close',#終値
+            'Volume'#出来高
+        ] #OHLCV
+        candle = [
+            'Datetime', #日時
+            'Direction',#方向性
+            'Body',     #実体
+            'pShadow',  #上ヒゲ
+            'mShadow',  #下ヒゲ
+        ] #ローソク足
+        bb = [
+            'Datetime', #日時
+            'SMA',  #単純移動平均
+            'Std',  #標準偏差
+            'pBB',  #上部ボリンジャーバンド(+σ)
+            'mBB'   #下部ボリンジャーバンド(-σ)
+        ] #ボリンジャーバンド
+        ps = [
+            'Datetime', #日時
+            'EP',       #極大値
+            'AF',       #加速因数
+            'SAR',      #トレンド転換点
+        ] #パラボリックSAR
+        dmi = [
+            'Datetime', #日時
+            'pDM',  #
+            'mDM',  #
+            'TR'    #変動幅
+            'pDI',  #正の方向性指数
+            'mDI',  #負の方向性指数
+            'DX',   #方向性指数
+            'ADX',  #平均方向性指数
+        ] #方向性指数
+        rsi = [
+            'Datetime', #日時
+            'Diff', #前日差分
+            'pDiff',#正の前日差分
+            'mDiff',#負の前日差分
+            'pAve', #正の平均
+            'mAve', #負の平均
+            'RSI'   #相対力指数
+        ] #相対力指数
+        rci = [
+            'Datetime', #日時
+            'TimeRank', #日付順位
+            'PriceRank',#価格順位
+            'RankSquare',#順位差の2乗
+            'd',        #
+            'RCI',      #
+        ]
+        macd = [
+            'Datetime', #日時
+            'ShortEMA', #短期指数平滑移動平均
+            'LongEMA',  #長期指数平滑移動平均
+            'MACD',     #移動平均収束拡散
+            'Signal',   #シグナル
+            'Hist',     #ヒストグラム
+            'Crossover' #交差
+        ] #移動平均収束拡散
 
     def __init__(
         self,
-        pair='BTCUSD',
         time_scale=1,
     ):
-        self.pair = pair
-        self.time_scale = time_scale
+        self.time_scale = time_scale #時間足　
+        self.ohlcv = pd.DataFrame(columns=self.Column.ohlcv)
         self.candle = pd.DataFrame(columns=self.Column.candle)
         self.bb = pd.DataFrame(columns=self.Column.bb)
+        self.ps = pd.DataFrame(columns=self.Column.ps)
+        self.dmi = pd.DataFrame(columns=self.Column.dmi)
         self.rsi = pd.DataFrame(columns=self.Column.rsi)
+        self.rci = pd.DataFrame(columns=self.Column.rci)
         self.macd = pd.DataFrame(columns=self.Column.macd)
 
-    def add_candle(
+    def add_ohlcv(
         self,
-        last_row,
+        ohlcv_dict,
     ):
-        self.candle = self.candle.append(last_row, ignore_index=True)
-        self.candle = self.candle.drop_duplicates()
-        print(len(self.candle))
-        if 180 <= len(self.candle):
-            self.candle = self.candle[-179:]
+        self.ohlcv = self.ohlcv.append(ohlcv_dict, ignore_index=True)
+
+    def initialize_bb(
+        self,
+        term=20,
+        coefficient=2
+    ):
+        self.bb['Datetime'] = self.ohlcv['Datetime']
+        self.bb['SMA'] = self.ohlcv['Close'].rolling(term).mean()
+        self.bb['Std'] = self.ohlcv['Close'].rolling(term).std()
+        self.bb['pBB'] = self.bb['SMA'] + coefficient*self.bb['Std']
+        self.bb['mBB'] = self.bb['SMA'] - coefficient*self.bb['Std']
 
     def calculate_bb(
         self,
         term=20,
         coefficient=2
     ):
-        self.bb['Date'] = self.candle['Date']
-        self.bb['SMA'] = self.candle['Close'].rolling(term).mean()
-        self.bb['Std'] = self.candle['Close'].rolling(term).std()
-        self.bb['+2σ'] = self.bb['SMA'] + coefficient*self.bb['Std']
-        self.bb['-2σ'] = self.bb['SMA'] - coefficient*self.bb['Std']
-        if 180 <= len(self.bb):
-            self.bb = self.bb[-179:]
-        
-    def calculate_rsi(
+        datetime_ = self.ohlcv['Datetime'].iloc[-1]
+        close_list = self.ohlcv['Close'][-term:]
+        sma = close_list.mean()
+        std = close_list.std()
+        p_bb = sma+coefficient*std
+        m_bb = sma-coefficient*std
+        bb_dict = {
+            'Datetime':datetime_,
+            'SMA':sma,
+            'Std':std,
+            'pBB':p_bb,
+            'mBB':m_bb,
+        }
+        self.bb = self.ohlcv.append(bb_dict, ignore_index=True)
+
+    def initialize_ps(
+        self,
+        max_af=0.2,
+    ):
+        self.bull = True
+        high_ = self.ohlcv['High'].iloc[0]
+        low_ = self.ohlcv['Low'].iloc[0]
+        self.ep = high_
+        self.af = self.initial_af = 0.02
+        self.ep_list = [self.ep]
+        self.af_list = [self.af]
+        self.sar_list = [self.ohlcv['Low'][0]]
+        for counter in range(len(self.ohlcv)):
+            high_ = self.ohlcv['High'].iloc[counter]
+            low_ = self.ohlcv['Low'].iloc[counter]
+            parabolic_ = self.sar_list[-1]
+            if self.bull and (parabolic_ > low_):
+                self.ps_is_touched=  True
+            elif not self.bull and (parabolic_ < high_):
+                self.ps_is_touched = True
+            else:
+                self.ps_is_touched = False
+            if self.ps_is_touched == True:
+                self.sar = self.ep
+                self.af = self.initial_af
+                if self.bull:
+                    self.bull = False
+                    self.ep = low_
+                else:
+                    self.bull = True
+                    self.ep = high_
+            else:
+                if self.bull and self.ep < high_:
+                    self.ep = high_
+                    self.af = min(self.af + self.initial_af, max_af)
+                elif not self.bull and self.ep > low_:
+                    self.ep = low_
+                    self.af = min(self.af + self.initial_af, max_af)
+                self.sar = self.sar_list[-1] + self.af * (self.ep - self.sar_list[-1])      
+            if counter == 0:
+                self.ep_list[-1] = self.ep
+                self.af_list[-1] = self.af
+                self.sar_list[-1] = self.sar
+            else:
+                self.ep_list.append(self.ep)
+                self.af_list.append(self.af)
+                self.sar_list.append(self.sar)
+        self.ps['Datetime'] = self.ohlcv['Datetime']
+        #self.ps['EP'] = ep = pd.Series([self.ep_list], index=['EP'])
+        #self.ps['AF'] = af = pd.Series([self.af_list], index=['AF'])
+        #self.ps['SAR'] = sar = pd.Series([self.sar_list], index=['SAR'])
+        self.ps['EP'] = self.ep_list
+        self.ps['AF'] = self.af_list
+        self.ps['SAR'] = self.sar_list
+
+    def calculate_ps(
+        self,
+        max_af=0.2,
+    ):
+        datetime_ = self.ohlcv['Datetime'].iloc[-1]
+        high_ = self.ohlcv['High'].iloc[-1]
+        low_ = self.ohlcv['Low'].iloc[-1]
+        parabolic_ = self.sar_list[-1]
+        if self.bull and (parabolic_ > low_): #強気相場かつパラボリックが安値以上
+            self.ps_is_touched=  True
+        elif not self.bull and (parabolic_ < high_): #弱気相場かつパラボリックが高値以下
+            self.ps_is_touched = True
+        else:
+            self.ps_is_touched = False
+        if self.ps_is_touched == True: #パラボリックが触れた場合
+            self.sar_list = self.ep
+            self.af = self.initial_af
+            if self.bull: #強気相場の場合
+                self.bull = False
+                self.ep = low_
+            else: #弱気相場の場合
+                self.bull = True
+                self.ep = high_
+        else: #パラボリックが触れていない場合
+            if self.bull and self.ep < high_: #強気相場かつ極大値が高値以下の場合
+                self.ep = high_
+                self.af = min(self.af + self.initial_af, max_af)
+            elif not self.bull and self.ep > low_: #弱気相場かつ極大値以上の場合
+                self.ep = low_
+                self.af = min(self.af + self.initial_af, max_af)
+            self.sar_list = self.sar_list + self.af * (self.ep - self.sar_list)        
+        ps_dict = {
+            'Datetime':datetime_,
+            'EP':self.ep,
+            'AF':self.af,
+            'SAR':self.temp_sar,
+        }
+        self.ps = self.ps.append(ps_dict, ignore_index=True)
+
+    def initialize_rsi(
         self,
         term=14
     ):
-        self.rsi['Date'] = self.candle['Date']
-        self.rsi['Diff'] = self.candle['Close'].diff()
+        self.rsi['Datetime'] = self.ohlcv['Datetime']
+        self.rsi['Diff'] = self.ohlcv['Close'].diff()
         up = self.rsi['Diff'].copy(); up[up < 0] = 0
         down = self.rsi['Diff'].copy(); down[down > 0] = 0
-        self.rsi['+Ave'] = up.rolling(term).mean()
-        self.rsi['-Ave'] = down.abs().rolling(term).mean()
-        self.rsi['RSI'] = 100*self.rsi['+Ave']/(self.rsi['+Ave']+self.rsi['-Ave'])
-        if 180 <= len(self.rsi):
-            self.rsi = self.rsi[-179:]
+        self.rsi['pAve'] = up.rolling(term).mean()
+        self.rsi['mAve'] = down.abs().rolling(term).mean()
+        self.rsi['RSI'] = 100*self.rsi['pAve']/(self.rsi['pAve']+self.rsi['mAve'])
 
-    def calculate_macd(
+    def initialize_macd(
         self,
-        short_term=12, # 6,12,19
-        long_term=26, # 19,26,39
-        signal_term=9 # 4,9,12
+        short_term=12,
+        long_term=26,
+        signal_term=9
     ):
-        self.macd['Date'] = self.candle['Date']
-        self.macd['ShortMA'] = self.candle['Close'].ewm(short_term).mean().round(3)
-        self.macd['LongMA'] = self.candle['Close'].ewm(long_term).mean().round(3)
+        self.macd['Datetime'] = self.ohlcv['Datetime']
+        self.macd['ShortMA'] = self.ohlcv['Close'].ewm(short_term).mean()
+        self.macd['LongMA'] = self.ohlcv['Close'].ewm(long_term).mean()
         self.macd['MACD'] = self.macd['ShortMA'] - self.macd['LongMA']
-        self.macd['Signal'] = self.macd['MACD'].rolling(signal_term).mean().round(3)
+        self.macd['Signal'] = self.macd['MACD'].rolling(signal_term).mean()
         self.macd['Hist'] = self.macd['MACD'] - self.macd['Signal']
         diff = self.macd['Hist'].diff()
         up = diff.copy(); up[up < 0] = 0
@@ -74,7 +244,156 @@ class Crypto:
         self.pave = up.rolling(15).mean()
         self.mave = down.abs().rolling(15).mean()
         self.macd['HistRSI'] = 100*self.pave/(self.pave+self.mave)
-        if 180 <= len(self.macd):
-            self.macd = self.macd[-179:]
+
+    def calculate_candle(self):
+        datetime_ = self.ohlcv['Datetime'].iloc[-1]
+        open_ = self.ohlcv['Open'].iat[-1]
+        high_ = self.ohlcv['High'].iat[-1]
+        low_ = self.ohlcv['Low'].iat[-1]
+        close_ = self.ohlcv['Close'].iat[-1]
+        if open_ > close_:
+            direction = -1
+            body = open_ - close_
+            p_shadow = high_ - open_
+            m_shadow = close_ - low_
+        elif open_ < close_:
+            dirrection = 1
+            body = close_ - open_
+            p_shadow = high_ - close_
+            m_shadow = open_ - low_
+        else:
+            dirrection = 0
+            body = 0
+            p_shadow = high_ - open_
+            m_shadow = open_ - low_
+        candle_dict = {
+            'Datetime': datetime_,
+            'Dirrection': direction,
+            'Body': body,
+            'uShadow': p_shadow,
+            'mShadow': m_shadow,
+        }
+        self.candle = self.ohlcv.append(candle_dict, ignore_index=True)
+
+
+
+    def calculate_dmi(
+        self,
+        term=14,
+    ):
+        datetime_ = self.ohlcv['Datetime'].iloc[-1]
+        high_list = self.ohlcv['High'][-term:]
+        low_list = self.ohlcv['Low'][-term:]
+        #close_list = self.ohlcv['Close'][-term:]
+        close_list_shifted = self.ohlcv['Close'][-(term+1):].shift(1)[-term:]
+        p_dm = (high_list.iloc[-1] - high_list.iloc[-2])
+        m_dm = (low_list.iloc[-2] - low_list.iloc[-1])
+        if p_dm < 0:
+            p_dm = 0
+        elif (p_dm-m_dm) < 0:
+            p_dm = 0
+        if m_dm < 0:
+            m_dm = 0
+        elif (m_dm-p_dm) < 0:
+            m_dm = 0
+        a = (high_list - close_list_shifted).abs()
+        b = (close_list_shifted - low_list).abs()
+        c = (high_list - low_list).abs()
+        tr = pd.concat([a, b, c], axis=1).max(axis=1)
+        p_di = p_dm.sum()/100*tr.sum()
+        m_di = m_dm.sum()/100*tr.sum()
+        dx = (p_di-m_di).abs()/100*(p_di+m_di)
+        dx = dx.fillna(0)
+        adx = dx.ewm(span=term).mean()
+        dmi_dict = {
+            'Datetime':datetime_,
+            'pDM':p_dm,
+            'mDM':m_dm,
+            'TR':tr,
+            'pDI':p_di,
+            'mDI':m_di,
+            'DX':dx,
+            'ADX':adx,
+        }
+        self.dmi = self.ohlcv.append(dmi_dict, ignore_index=True)
+
+    def calculate_rsi(
+        self,
+        term=14,
+    ):
+        datetime_ = self.ohlcv['Datetime'].iloc[-1]
+        diff_ = self.ohlcv['Close'].iloc[-2] - self.ohlcv['Close'].iloc[-1]
+        if diff_ >= 0:
+            p_diff = diff_
+            m_diff = 0
+        elif diff_ > 0:
+            p_diff = 0
+            m_diff = diff_
+        p_diff_list = self.rsi['pDiff'][-(term-1):]
+        m_diff_list = self.rsi['mDiff'][-(term-1):]
+        p_diff_list = p_diff_list.append(p_diff, index=['pDiff'], ignore_index=True)
+        m_diff_list = m_diff_list.append(m_diff, index=['mDiff'], ignore_index=True)
+        p_ave = p_diff_list.mean()
+        m_ave = m_diff_list.abs().mean()
+        rsi = 100*p_ave/(p_ave+m_ave)
+        rsi_dict = {
+            'Datetime':datetime_,
+            'Diff':diff_,
+            'pDiff':p_diff,
+            'mDiff':m_diff,
+            'pAve':p_ave,
+            'mAve':m_ave,
+            'RSI':rsi,
+        }
+        self.rsi = self.rsi.append(rsi_dict, ignore_index=True)
+
+    def calculate_rci(
+        self,
+        term=14,
+    ):
+        pass
+
+    def calculate_macd(
+        self,
+        short_term=12, # 6,12,19
+        long_term=26, # 19,26,39
+        signal_term=9, # 4,9,12
+    ):
+        datetime_ = self.ohlcv['Date'].iloc[-1]
+        close_list = self.ohlcv['Close'][-long_term:]
+        short_ema = close_list.ewm(short_term).mean().iloc[-1]
+        long_ema = close_list.ewm(long_term).mean().iloc[-1]
+        macd = short_ema - long_ema
+        macd_list = self.macd['MACD'][-(signal_term-1):]
+        macd_list = macd_list.append(macd, index=['MACD'], ignore_index=True)
+        signal = macd_list.mean()
+        hist = macd - signal
+        before_hist = self.macd['Hist'].iloc[-2]
+        if hist > before_hist:
+            crossover = 1
+        elif hist < before_hist:
+            crossover = -1
+        else:
+            crossover = 0
+        macd_dict = {
+            'Datetime':datetime_,
+            'ShortEMA':short_ema,
+            'LongEMA':long_ema,
+            'MACD':macd,
+            'Signal':signal,
+            'Hist':hist,
+            'Crossover':crossover,
+        }
+        self.macd = self.macd.append(macd, ignore_index=True)
+
+    def trim(
+        self,
+        df,
+        term
+    ):
+        if term < len(df):
+            df = df.drop_duplicates()
+            df = df[-(term-1):]
+        return df
 
 
--- orderer.py
+++ orderer.py
@@ -26,18 +26,11 @@ class Orderer:
         data_range=3*60*60,
     ):
         self.pair = pair
-        self.latest_time = None
-        self.latest_price = None # 最新終値
-        self.balance_jpy = None # 円残高(価格)
-        self.balance_btc = None # 残高量(量)
-        self.ask_price = self.ask_amount = None # 買値
-        self.bid_price = self.bid_amount = None #　売値
         self.time_scale_list = time_scale_list # 時間足リスト
         self.data_range = data_range # データ範囲
         self.crypto_list = []
         for counter in range(len(self.time_scale_list)):
             crypto_ = crypto.Crypto(
-                pair=self.pair,
                 time_scale=self.time_scale_list[counter],
             )
             self.crypto_list = self.crypto_list+[crypto_]
@@ -67,17 +60,6 @@ class Orderer:
             LINE_API_TOKEN,
         )
 
-    def initialize_data(
-        self,
-    ):
-        for counter in range(len(self.time_scale_list)):
-            start_time = int(time.time()) - self.time_scale_list[counter] * self.data_range
-            self.collect_data(
-                index=counter,
-                start_time=start_time
-            )
-        print(self.crypto_list[0].candle.values[-2:])
-
     def collect_data(
         self,
         index,
@@ -87,35 +69,22 @@ class Orderer:
             start_time=start_time,
             time_scale=self.time_scale_list[index],
         )
-        result_balance = self.coincheck_client.get_balance()
+        #result_balance = self.coincheck_client.get_balance()
         for counter in range(len(result_candle_list)):
             result_candle = result_candle_list[counter]
             result_candle_reshaped = {
-                'Date': datetime.datetime.fromtimestamp(result_candle['open_time']),
+                'Datetime': datetime.datetime.fromtimestamp(result_candle['open_time']),
                 'Open': float(result_candle['open']),
                 'High': float(result_candle['high']),
                 'Low': float(result_candle['low']),
                 'Close': float(result_candle['close']),
                 'Volume': int(result_candle['volume']),
             }
-            self.crypto_list[index].add_candle(last_row=result_candle_reshaped)
-        
-            result_pal_reshaped = {
-                'Date': datetime.datetime.fromtimestamp(result_candle['open_time']),
-                'JPY': float(result_balance['jpy']),
-                'BTC': float(result_balance['btc']),
-            }
-
-        self.crypto_list[index].calculate_bb()
-        self.crypto_list[index].calculate_rsi()
-        self.crypto_list[index].calculate_macd()
-        self.latest_time = self.crypto_list[0].candle.at[179, 'Date']
-        self.latest_price = self.crypto_list[0].candle.at[179, 'Close']
-
-    def predict_data(
-        self,
-    ):
-        pass
+            self.crypto_list[index].add_ohlcv(ohlcv_dict=result_candle_reshaped)
+            self.crypto_list[index].calculate_bb()
+            self.crypto_list[index].calculate_ps()
+            self.crypto_list[index].calculate_rsi()
+            self.crypto_list[index].calculate_macd()
 
     def plot_graph(
         self,
@@ -126,29 +95,27 @@ class Orderer:
         for cnt in range(len(self.time_scale_list)): #reversedへ変更
             rcnt = len(self.time_scale_list)-1-cnt
             term = int(self.data_range/60/self.time_scale_list[rcnt])
-            _datetime = self.crypto_list[rcnt].candle['Date'][-term:]
-            _high = self.crypto_list[rcnt].candle['High'][-term:]
-            _low = self.crypto_list[rcnt].candle['Low'][-term:]
-            _bbminus = self.crypto_list[rcnt].bb['-2σ'][-term:]
-            _bbplus = self.crypto_list[rcnt].bb['+2σ'][-term:]
+            _datetime = self.crypto_list[rcnt].ohlcv['Datetime'][-term:]
+            _high = self.crypto_list[rcnt].ohlcv['High'][-term:]
+            _low = self.crypto_list[rcnt].ohlcv['Low'][-term:]
+            _bbminus = self.crypto_list[rcnt].bb['mBB'][-term:]
+            _bbplus = self.crypto_list[rcnt].bb['pBB'][-term:]
+            _ps = self.crypto_list[rcnt].ps['SAR'][-term:]
             _hist = self.crypto_list[rcnt].macd['Hist'][-term:]
-            _hrsi = self.crypto_list[rcnt].macd['HistRSI'][-term:]
             _rsi = self.crypto_list[rcnt].rsi['RSI'][-term:]
             self.axes[0].plot(_datetime, _high, color=COLOR.candle[0], alpha=ALPHA[rcnt])
             self.axes[0].plot(_datetime, _low, color=COLOR.candle[1], alpha=ALPHA[rcnt])
+            self.axes[0].scatter(_datetime, _ps, alpha=ALPHA[rcnt])
             self.axes[0].fill_between(_datetime, _bbminus, _bbplus, facecolor=COLOR.bb[cnt], alpha=ALPHA[rcnt])
             self.axes[1].fill_between(_datetime, _hist, 0, facecolor=COLOR.macd[cnt], alpha=ALPHA[rcnt])
             self.axes[2].plot(_datetime, _rsi, color=COLOR.rsi[cnt], alpha=ALPHA[rcnt])
-        self.axes[2].fill_between(self.crypto_list[rcnt].candle['Date'], 0, 25, facecolor=COLOR.bb[0], alpha=3/10)
-        self.axes[2].fill_between(self.crypto_list[rcnt].candle['Date'], 75, 100, facecolor=COLOR.bb[0], alpha=3/10)
+        self.axes[2].fill_between(self.crypto_list[rcnt].ohlcv['Datetime'], 0, 25, facecolor=COLOR.bb[0], alpha=3/10)
+        self.axes[2].fill_between(self.crypto_list[rcnt].ohlcv['Datetime'], 75, 100, facecolor=COLOR.bb[0], alpha=3/10)
         self.axes[0].grid(alpha=1/4)
         self.axes[1].grid(alpha=1/4)
         self.axes[2].grid(alpha=1/4)
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
-
-    def judge_trade(self):
-        pass
 
     def buy(self):
         result = self.coincheck_client.post_order(
@@ -166,11 +133,35 @@ class Orderer:
         )
         self.line_client.send_message(message=result)
 
+    def initialize_data(
+        self,
+    ):
+        for i in range(len(self.time_scale_list)):
+            start_time = int(time.time())-self.time_scale_list[i]*self.data_range
+            result_ohlcv_list = self.bybit_client.fetch_candle(
+                start_time=start_time,
+                time_scale=self.time_scale_list[i],
+            )
+            for j in range(len(result_ohlcv_list)):
+                result_ohlcv = result_ohlcv_list[j]
+                ohlcv_dict = {
+                    'Datetime': datetime.datetime.fromtimestamp(result_ohlcv['open_time']),
+                    'Open': float(result_ohlcv['open']),
+                    'High': float(result_ohlcv['high']),
+                    'Low': float(result_ohlcv['low']),
+                    'Close': float(result_ohlcv['close']),
+                    'Volume': int(result_ohlcv['volume']),
+                }
+                self.crypto_list[i].add_ohlcv(ohlcv_dict=ohlcv_dict)
+            self.crypto_list[i].initialize_bb()
+            self.crypto_list[i].initialize_ps()
+            self.crypto_list[i].initialize_rsi()
+            self.crypto_list[i].initialize_macd()
+
     def mainloop(self):
         while True:
             now = datetime.datetime.now()
-            last = self.latest_time
-            start_time = datetime.datetime(last.year, last.month, last.day, last.hour, last.minute, 0)
+            start_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, 0)-datetime.timedelta(minutes=1)
             end_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, 0)+datetime.timedelta(minutes=1)
             while datetime.datetime.now() < end_time:
                 plt.pause(3)
@@ -180,7 +171,6 @@ class Orderer:
                     start_time=int(start_time.timestamp())
                 )
             print(self.crypto_list[0].candle.values[-2:])
-            self.judge_trade()
             self.plot_graph()
 
 def main():
